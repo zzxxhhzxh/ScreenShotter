52b31c556190a97ef6812a1d2c203cd78bc0b6f8
Xiaohuan Zhou <42593813+zzxxhhzxh@users.noreply.github.com>
2020-07-29 12:24:34 -0500

    Initial commit

---
+++ frmMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.IO;
+
+namespace ScreenShotter
+{
+    public partial class frmMain : Form
+    {
+        Bitmap img, shotImg, cache;
+        Rectangle shotRect;
+        Point p; // start point
+        readonly Pen dPen = Pens.DodgerBlue;
+        readonly Brush dBrush = new SolidBrush(Color.FromArgb(63, 31, 63, 63));
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            var screenBounds = Screen.AllScreens[Screen.AllScreens.Length - 1].Bounds;
+            Bounds = screenBounds;
+            img = new Bitmap(screenBounds.Width, screenBounds.Height);
+            using (var g = Graphics.FromImage(img))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, screenBounds.Size);
+            }
+            cache = new Bitmap(img);
+        }
+
+        private void FrmMain_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+
+        private void FrmMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)
+                Application.Exit();
+        }
+
+        private void FrmMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = e.Location;
+            MouseMove += new MouseEventHandler(FrmMain_MouseMove);
+        }
+
+        private void FrmMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            img = new Bitmap(cache);
+            shotRect = new Rectangle(p.X, p.Y, e.X - p.X, e.Y - p.Y);
+            using (var g = Graphics.FromImage(img))
+            {
+                g.CompositingMode = CompositingMode.SourceOver;
+                g.FillRectangle(dBrush, shotRect);
+                g.DrawRectangle(dPen, shotRect);
+            }
+            Refresh();
+        }
+
+        private void FrmMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            MouseMove -= new MouseEventHandler(FrmMain_MouseMove);
+            img = new Bitmap(cache);
+            Refresh();
+            if (shotRect.Width == 0 || shotRect.Height == 0)
+                return;
+            Cursor = Cursors.Default;
+            GetScreenShot(shotRect);
+            shotRect = new Rectangle();
+            Cursor = Cursors.Cross;
+        }
+
+        private void GetScreenShot(Rectangle rect)
+        {
+            shotImg = img.Clone(rect, PixelFormat.DontCare);
+            Clipboard.SetImage(shotImg);
+            string name = $"Screenshot_{DateTime.Now:MMddyy_HHmmss}.png";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            shotImg.Save(Path.Combine(path, name), ImageFormat.Png);
+        }
+    }
+}
---
+++ frmMain.designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ScreenShotter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FrmMain_Paint);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.FrmMain_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FrmMain_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FrmMain_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
